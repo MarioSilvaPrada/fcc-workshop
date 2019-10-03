0d68f49531bf64459f3e2a874162bec719532810
Mário Prada <mariosilvaprada@gmail.com>
2019-10-03 23:40:03 +0100

    adding class component

--- src/App.js
+++ src/App.js
@@ -6,33 +6,26 @@ import dogs from './data/dogs';
 import DogSelected from './components/DogSelected/DogSelected';
 
 class App extends React.Component {
-  // const [ dog, selectDog ] = useState('');
   state = {
-    dog: ''
-  }
-  
+    dog: '',
+  };
   render() {
     return (
       <div className='App'>
         <div className='all-dogs'>
           {dogs.map((dog, i) => (
-            <div key={i} onClick={() => this.setState({dog: dog.name})} className='dog-card'>
+            <div key={i} onClick={() => this.setState({ dog: dog.name })} className='dog-card'>
               <h1>Name: {dog.name}</h1>
               <p>Breed: {dog.breed}</p>
               <p>Age: {dog.age}</p>
             </div>
           ))}
         </div>
-        
-        <DogSelected name={this.state.dog}/> 
-  
+
+        <DogSelected name={this.state.dog} />
       </div>
     );
   }
-  
-};
+}
 
 export default App;
-
-
-
