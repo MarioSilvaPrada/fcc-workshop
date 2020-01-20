c1793f3d192ce5649288f51f0ca795fc92b30f65
Mário Prada <mariosilvaprada@gmail.com>
2020-01-20 22:15:53 +0000

    adding boilerplate

--- src/components/DogSelected/DogSelected.js
+++ src/components/DogSelected/DogSelected.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './DogSelected.css';
 
-const DogSelected = ({name}) => {
-    if(name) {
-        return <h1 className='isSelected'>You just selected {name}</h1>
+const DogSelected = (props) => {
+    if(props.name) {
+        return <h1 className='isSelected'>You just selected {props.name}</h1>
     }
     else {
         return <h1 className='isNotSelected'>Please select your dog</h1>
